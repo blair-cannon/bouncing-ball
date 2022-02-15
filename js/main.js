dd27252f137e7f8d5cda6b8dab2d4779d6f2880f
Blair Preston <blair.preston@student.awesomeinc.org>
2022-02-15 18:16:26 +0000

    update js

--- js/main.js
+++ js/main.js
@@ -93,7 +93,7 @@ while (balls.length < 25) {
         random(0 + size,height - size),
         random(-7,7),
         random(-7,7),
-        randomRBG(),
+        randomRGB(),
         size 
     );
 
