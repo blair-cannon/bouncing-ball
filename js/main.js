511ef1b1bcaf615097dfc2a978c0ab10f60429b9
Blair Preston <blair.preston@student.awesomeinc.org>
2022-02-15 18:28:26 +0000

    update js

--- js/main.js
+++ js/main.js
@@ -12,14 +12,13 @@ const height = canvas.height = window.innerHeight;
 //function to generate random number
 
 function random(min, max) {
-    const num = Math.floor(Math.random() * (max - min +1)) + min;
-    return num;
+    return Math.floor(Math.random() * (max - min +1)) + min;
 }
 
 //function to generate random color
 
 function randomRGB() {
-    return `rgb(${random(0,255)},${random(0, 255)},${random(0.255)})`;
+    return `rgb(${random(0, 255)},${random(0, 255)},${random(0.255)})`;
 }
 
 class Ball {
